21b4baf993f6e838fe16aa1281c70ad0869400e5
Ashutosh Kumar <145090058+official-ashutosh@users.noreply.github.com>
2025-06-29 23:53:20 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3316-find-the-sum-of-subsequence-powers/3316-find-the-sum-of-subsequence-powers.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+
+    int dp[55][55][55][55];
+
+    static const int mod = 1e9+7;
+
+    int func(int id, int lst, int ct, int prev1, int prev2, vector<int> &nums){
+        if(ct == 0) {
+            // cout << prev1 << " " << prev2 << endl;
+            return abs(nums[prev1]-nums[prev2]);
+        }
+
+        if(id == nums.size() || ct < 0){
+            return 0;
+        }
+
+        if(dp[lst+1][ct][prev1+1][prev2+1] != -1) return dp[lst+1][ct][prev1+1][prev2+1];
+
+        long long ans = 0;
+
+        ans = (ans+func(id+1, lst, ct, prev1, prev2, nums))%mod;
+
+        // cout << id << " " << func(id+1, ct, prev1, prev2, nums) << endl;
+
+        if(lst == -1){
+            ans = (ans+func(id+1, id, ct-1, prev1, prev2, nums)) % mod;
+
+            // cout << id << func(id+1, ct-1, id+1, prev2, nums) << endl;
+            
+        } else {
+            if(prev1 == -1 || prev2 == -1 || abs(nums[id]-nums[lst]) < abs(nums[prev1]-nums[prev2]) ){
+                // cout << id << " " << abs(nums[id]-nums[prev2-1]) << endl;
+                ans = (ans+func(id+1, id, ct-1, lst, id, nums)) % mod;
+
+                cout << id << " " <<  ans;
+                // cout << func(id+1, ct-1, prev2, id+1, nums) << endl;
+            } else {
+                ans = (ans+func(id+1, id, ct-1, prev1, prev2, nums)) % mod;
+                cout << id << " " << ans << endl;
+            }
+        }
+
+        return dp[lst+1][ct][prev1+1][prev2+1] = ans;
+    }
+
+    int sumOfPowers(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+
+        memset(dp, -1, sizeof(dp));
+
+        return func(0, -1, k, -1, -1, nums);
+    }
+};
