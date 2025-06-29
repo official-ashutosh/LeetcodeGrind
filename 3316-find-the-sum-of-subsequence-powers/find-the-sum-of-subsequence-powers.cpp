1e2f34f69656bfd614d6c8bed68c0e52eb8f8781
Ashutosh Kumar <145090058+official-ashutosh@users.noreply.github.com>
2025-06-29 23:55:31 +0530

    Time: 919 ms (36.84%) | Memory: 52.8 MB (52.63%) - LeetSync

--- 3316-find-the-sum-of-subsequence-powers/find-the-sum-of-subsequence-powers.cpp
+++ 3316-find-the-sum-of-subsequence-powers/find-the-sum-of-subsequence-powers.cpp
@@ -1,30 +1,44 @@
 class Solution {
 public:
-    int mod = 1e9+7;
-    int dp[52][52][52][51];
-    
-    int solve(int i, int last, int ind1, int ind2, int k, vector<int>& nums){
-        
-        if(k == 0)return abs(nums[ind1]-nums[ind2]);
-        if(i>=nums.size())return 0;
-        
-        if(dp[last+1][ind1+1][ind2+1][k]!=-1)return dp[last+1][ind1+1][ind2+1][k];
-        
-        int temp1 = ind1, temp2 = ind2;
-        
-        if(last != -1 && (ind1 == -1 || ind2 == -1 || abs(nums[ind1]-nums[ind2])>abs(nums[i]-nums[last]))){
-            temp1 = last;
-            temp2 = i;
+
+    int dp[52][52][52][52];
+
+    static const int mod = 1e9+7;
+
+    int func(int id, int lst, int ct, int prev1, int prev2, vector<int> &nums){
+        if(ct == 0) {
+            return abs(nums[prev1]-nums[prev2]);
         }
-        
-        int op1 = solve(i+1,i,temp1,temp2,k-1,nums)%mod;
-        int op2 = solve(i+1,last,ind1,ind2,k,nums)%mod;
-        
-        return dp[last+1][ind1+1][ind2+1][k] = (op1 + op2)%mod;
+
+        if(id == nums.size() || ct < 0){
+            return 0;
+        }
+
+        if(dp[lst+1][ct][prev1+1][prev2+1] != -1) return dp[lst+1][ct][prev1+1][prev2+1];
+
+        long long ans = 0;
+
+        ans = (ans+func(id+1, lst, ct, prev1, prev2, nums))%mod;
+
+        if(lst == -1){
+            ans = (ans+func(id+1, id, ct-1, prev1, prev2, nums)) % mod;
+            
+        } else {
+            if(prev1 == -1 || prev2 == -1 || abs(nums[id]-nums[lst]) < abs(nums[prev1]-nums[prev2]) ){
+                ans = (ans+func(id+1, id, ct-1, lst, id, nums)) % mod;
+            } else {
+                ans = (ans+func(id+1, id, ct-1, prev1, prev2, nums)) % mod;
+            }
+        }
+
+        return dp[lst+1][ct][prev1+1][prev2+1] = ans;
     }
+
     int sumOfPowers(vector<int>& nums, int k) {
-        memset(dp,-1,sizeof(dp));
         sort(nums.begin(), nums.end());
-        return solve(0,-1,-1,-1,k,nums);
+
+        memset(dp, -1, sizeof(dp));
+
+        return func(0, -1, k, -1, -1, nums);
     }
 };
