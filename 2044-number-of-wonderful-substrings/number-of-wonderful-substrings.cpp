c0863e56d33b9420135be6e520861d6d3686fa06
Ashutosh Kumar <145090058+official-ashutosh@users.noreply.github.com>
2025-07-20 12:03:09 +0530

    Time: 534 ms (10.27%) | Memory: 20.8 MB (41.69%) - LeetSync

---
+++ 2044-number-of-wonderful-substrings/number-of-wonderful-substrings.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    long long wonderfulSubstrings(string word) {
+        map<int, long long> mp;
+
+        long long ans = 0;
+        int p = 0;
+        mp[0] = 1;
+
+        for(auto i : word){
+            int c = i-'a';
+
+            p ^= (1<<c);
+            ans += mp[p];  // sare even ke liye
+
+            for(int i=0; i<10; i++){
+                ans += mp[p^(1<<i)];  // 1 odd 
+            }
+
+            mp[p]++;
+        }
+
+        return ans;
+    }
+};
