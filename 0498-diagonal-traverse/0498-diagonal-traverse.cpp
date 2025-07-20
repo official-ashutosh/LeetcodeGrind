acb9565dea364f2a85dc0f9265516f2de674a6b2
Ashutosh Kumar <145090058+official-ashutosh@users.noreply.github.com>
2025-07-20 15:17:02 +0530

    Time: 25 ms (32.22%), Space: 22.9 MB (60.09%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.cpp
+++ 0498-diagonal-traverse/0498-diagonal-traverse.cpp
@@ -9,12 +9,12 @@ public:
         for(int i=0; i<=(m-1+n-1); i++){
 
             if(i % 2){
-                for(int j=0; j<=i; j++){
+                for(int j=0; j<min(i+1, n); j++){
                     if(j < n && (i-j < m))
                     ans.push_back(mat[j][i-j]);
                 }
             } else {
-                for(int j=i; j>=0; j--){
+                for(int j=min(n-1, i); j>=0; j--){
                     if(j < n && (i-j < m))
                     ans.push_back(mat[j][i-j]);
                 }
