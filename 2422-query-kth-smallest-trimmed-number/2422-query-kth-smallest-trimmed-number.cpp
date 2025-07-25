8523fefd511487b4368be93a6a5a18084fb202ac
Ashutosh Kumar <145090058+official-ashutosh@users.noreply.github.com>
2025-07-25 19:10:02 +0530

    Time: 241 ms (90.84%), Space: 43 MB (93.26%) - LeetHub

--- 2422-query-kth-smallest-trimmed-number/2422-query-kth-smallest-trimmed-number.cpp
+++ 2422-query-kth-smallest-trimmed-number/2422-query-kth-smallest-trimmed-number.cpp
@@ -1,16 +1,34 @@
 class Solution {
 public:
     vector<int> smallestTrimmedNumbers(vector<string>& nums, vector<vector<int>>& queries) {
-        vector<int> res;
-        int n = nums.size();
-        for(auto &q:queries) {
-            vector<pair<string,int>> pq;
-            for(int i=0;i<n; i++) {
-                pq.push_back({(nums[i].substr(nums[i].size()-q[1])), i});
-            }
-            sort(pq.begin(), pq.end());
-            res.push_back(pq[q[0]-1].second);
+        int m = nums.size();
+        int L = nums[0].size();
+        
+        vector<vector<int>> v(L + 1, vector<int>(m));
+        for(int trim=1; trim<=L; ++trim) {
+            // initialize indices 0..m-1
+            for(int i=0; i<m; ++i) 
+                v[trim][i] = i;
+            
+            // sort by trimmed suffix, then by index
+            sort(v[trim].begin(), v[trim].end(),
+                 [&](int a, int b) {
+                     // compare last 'trim' chars
+                     const string &A = nums[a], &B = nums[b];
+                     int start = L - trim;
+                     for (int k = start; k < L; ++k) {
+                         if (A[k] != B[k]) 
+                             return A[k] < B[k];
+                     }
+                     return a < b;
+                 });
         }
-        return res;
+        
+        vector<int> ans;
+        for(auto &q : queries) {
+            int k = q[0], trim = q[1];
+            ans.push_back(v[trim][k - 1]);
+        }
+        return ans;
     }
 };
