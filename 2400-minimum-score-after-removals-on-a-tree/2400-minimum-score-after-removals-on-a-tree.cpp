63aa90c43b5e5a5fbdc23eeaa94eb4e5168f3c64
Ashutosh Kumar <145090058+official-ashutosh@users.noreply.github.com>
2025-07-25 12:02:08 +0530

    Time: 1822 ms (8.39%), Space: 20.3 MB (45.04%) - LeetHub

--- 2400-minimum-score-after-removals-on-a-tree/2400-minimum-score-after-removals-on-a-tree.cpp
+++ 2400-minimum-score-after-removals-on-a-tree/2400-minimum-score-after-removals-on-a-tree.cpp
@@ -1,68 +1,61 @@
 class Solution {
 public:
-    vector<int> adj[100010];  // adjacency list
-    vector<int> subtreeXor;   // xor of subtree rooted at each node
-    vector<int> parent;       // parent for each node
-    int totalXor = 0;         // xor of the entire tree
+    vector<int> g[100010];  
+    vector<int> subXor;  
+    vector<int> par;       
 
     void dfs(int u, int p, vector<int>& nums) {
-        subtreeXor[u] = nums[u];
-        parent[u] = p;
-        for (int v : adj[u]) {
-            if (v != p) {
+        subXor[u] = nums[u];
+        par[u] = p;
+        for(auto v : g[u]) {
+            if(v != p) {
                 dfs(v, u, nums);
-                subtreeXor[u] ^= subtreeXor[v];
+                subXor[u] ^= subXor[v];
             }
         }
     }
 
-    bool isDescendant(int u, int v) {
-        // Returns true if u is in the subtree of v
-        while (u != -1) {
-            if (u == v) return true;
-            u = parent[u];
+    bool check(int u, int v) {   // u child of v
+
+        while(u != -1) {
+            if(u == v) return true;
+            u = par[u];
         }
         return false;
     }
 
     int minimumScore(vector<int>& nums, vector<vector<int>>& edges) {
         int n = nums.size();
-        subtreeXor.resize(n);
-        parent.resize(n, -1);
+        subXor.resize(n);
+        par.resize(n, -1);
 
-        // Build tree
-        for (auto& e : edges) {
-            int u = e[0], v = e[1];
-            adj[u].push_back(v);
-            adj[v].push_back(u);
+        for(auto& i : edges) {
+            g[i[0]].push_back(i[1]);
+            g[i[1]].push_back(i[0]);
         }
 
-        // 1. Run DFS from node 0 to compute subtree XORs
         dfs(0, -1, nums);
-        totalXor = subtreeXor[0];
+        int totXor = subXor[0];
 
         int ans = INT_MAX;
 
-        // 2. Try every pair of edges (i, j)
-        for (int i = 1; i < n; ++i) {
-            for (int j = i + 1; j < n; ++j) {
-                int a = i, b = j;
+        for(int i=1; i<n; i++) {
+            for(int j=i+1; j<n; j++) {
 
-                // Ensure that we always treat a as lower in tree (child side)
-                if (isDescendant(a, b)) {
-                    int x = subtreeXor[a];
-                    int y = subtreeXor[b] ^ subtreeXor[a];
-                    int z = totalXor ^ subtreeXor[b];
+                if(check(i, j)) {
+                    int x = subXor[i];
+                    int y = subXor[j] ^ subXor[i];
+                    int z = totXor ^ subXor[j];
                     ans = min(ans, max({x, y, z}) - min({x, y, z}));
-                } else if (isDescendant(b, a)) {
-                    int x = subtreeXor[b];
-                    int y = subtreeXor[a] ^ subtreeXor[b];
-                    int z = totalXor ^ subtreeXor[a];
+                } else if(check(j, i)) {
+                    int x = subXor[j];
+                    int y = subXor[i] ^ subXor[j];
+                    int z = totXor ^ subXor[i];
                     ans = min(ans, max({x, y, z}) - min({x, y, z}));
                 } else {
-                    int x = subtreeXor[a];
-                    int y = subtreeXor[b];
-                    int z = totalXor ^ x ^ y;
+                    int x = subXor[i];
+                    int y = subXor[j];
+                    int z = totXor^x^y;
                     ans = min(ans, max({x, y, z}) - min({x, y, z}));
                 }
             }
