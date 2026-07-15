e0a2dfa3b90dadb6336d6586d42a3eca79809d04
Ashutosh Kumar <145090058+official-ashutosh@users.noreply.github.com>
2026-07-15 15:17:43 +0530

    Time: 99 ms (58.53%), Space: 63.1 MB (6.72%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -2,15 +2,17 @@ class Solution {
 public:
 
     vector<int> dp;
+    vector<vector<int>> pal;
 
     bool check(string &s, int i, int j){
-        while(i < j){
-            if(s[i] != s[j]) return false;
-            i++;
-            j--;
+        if(i >= j) return true;
+
+        if(pal[i][j] != -1) return pal[i][j];
+        if(s[i] == s[j]){
+            return pal[i][j] = check(s, i+1, j-1);
         }
 
-        return true;
+        return pal[i][j] = 0;
     }
 
     int func(int i, string &s){
@@ -31,6 +33,7 @@ public:
     int minCut(string s) {
         int n = s.size();
         dp.assign(n+1, -1);
+        pal.assign(n+1, vector<int>(n+1, -1));
 
         return func(0, s);
     }
