fb1b3f18ba19b2ebe17d654f659ed11564ab3ef7
Ashutosh Kumar <145090058+official-ashutosh@users.noreply.github.com>
2026-07-30 00:13:34 +0530

    Time: 993 ms (6.92%) | Memory: 61.5 MB (69.80%) - LeetSync

---
+++ 833-bus-routes/bus-routes.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int numBusesToDestination(vector<vector<int>>& routes, int source, int target) {
+        if(source == target) return 0;
+
+        map<int, vector<int>> g;
+        int n = routes.size();
+
+        for(int i=0; i<n; i++) {
+            for(auto j : routes[i]) {
+                g[j].push_back(i);
+            }
+        }
+
+        queue<int> q;
+        set<int> vis;
+        map<int, int> dist;
+
+        for(auto i : g[source]) {
+            q.push(i);
+            vis.insert(i);
+            dist[i] = 1;
+        }
+
+        while(!q.empty()) {
+            auto tp = q.front();
+            q.pop();
+
+            for(auto i : routes[tp]) {
+                if(i == target) return dist[tp];
+
+                for(auto child : g[i]) {
+                    if(!vis.count(child)) {
+                        vis.insert(child);
+                        q.push(child);
+                        dist[child] = dist[tp] + 1;
+                    }
+                }
+            }
+        }
+
+        return -1;
+    }
+};
