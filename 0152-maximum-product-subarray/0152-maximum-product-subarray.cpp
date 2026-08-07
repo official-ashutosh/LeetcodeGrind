62c74903763ee1753f996e44ada5990526e66b45
Ashutosh Kumar <145090058+official-ashutosh@users.noreply.github.com>
2026-08-07 11:30:29 +0530

    Time: 0 ms (100%), Space: 17.9 MB (5.85%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -9,7 +9,7 @@ public:
             p1 *= nums[i];
             p2 *= nums[i];
             p3 *= nums[n-1-i];
-            ans = max({ans, p1, p2, p3});
+            ans = max({ans, p1, p3});
             if(p3 == 0) p3 = 1;
             if(p1 == 0) p1 = 1;
             if(p2 <= 0) p2 = 1;
