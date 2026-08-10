bce409148c5dc969c21efcd3873cdebc07457ab0
Ashutosh Kumar <145090058+official-ashutosh@users.noreply.github.com>
2026-08-10 15:50:06 +0530

    Time: 24 ms (84.92%), Space: 85.5 MB (69.69%) - LeetHub

--- 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.cpp
+++ 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:  
     static int largestSubmatrix(vector<vector<int>>& matrix) {
-        int m=matrix.size(), n=matrix[0].size();
-        int area=count(matrix[0].begin(), matrix[0].end(), 1);
-
-        // if(m==1) return area;
-        // if(n==1){
-        //     for(int i=1; i<m; i++){
-        //         matrix[i][0]+=matrix[i][0]*matrix[i-1][0];
-        //         area=max(area, matrix[i][0]);
-        //     }
-        //     return area;
-        // }
+        int m = matrix.size(), n = matrix[0].size();
+        int ans = 0;
         
-        for(int i=1; i<m; i++){
+        for(int i=0; i<m; i++){
+
+            if(i > 0){
             for(int j=0;  j<n; j++){
                 matrix[i][j]+=matrix[i][j]*matrix[i-1][j];
+            }
             }
             const auto& row=matrix[i];
             int minH=i+1, maxH=0;
@@ -31,10 +25,10 @@ public:
             for(int x=maxH-minH; acc<n; x--){
                 if (freq[x]>0){
                     acc+=freq[x];
-                    area=max(area, acc*(x+minH));
+                    ans=max(ans, acc*(x+minH));
                 }
             }  
         }
-        return area;
+        return ans;
     }
 };
