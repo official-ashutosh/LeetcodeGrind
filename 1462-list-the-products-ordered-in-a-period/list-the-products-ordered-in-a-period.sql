055037a150fe3fb1e6d6795f183fc9d58ab4dd90
Ashutosh Kumar <145090058+official-ashutosh@users.noreply.github.com>
2026-08-29 16:35:35 +0530

    Time: 748 ms (74.81%) | Memory: 0B (100.00%) - LeetSync

--- 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
+++ 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-select product_name, sum(unit) as unit
+select p.product_name, sum(o.unit) as unit
 from Products p, Orders o
 where p.product_id = o.product_id 
 and month(order_date) = 2 and year(order_date) = 2020
