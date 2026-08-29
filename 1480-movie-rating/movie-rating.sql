92f6ea9c7ca5e2055e742dcc4ee5dde11b23df3a
Ashutosh Kumar <145090058+official-ashutosh@users.noreply.github.com>
2026-08-29 17:48:40 +0530

    Time: 1232 ms (93.46%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1480-movie-rating/movie-rating.sql
@@ -0,0 +1,24 @@
+# Write your MySQL query statement below
+select (
+    select u.name
+    from Users u
+    join MovieRating mr
+        on u.user_id = mr.user_id
+    group by u.user_id, u.name
+    order by COUNT(*) desc, u.name
+    limit 1
+) AS results
+
+union all
+
+select (
+    select m.title
+    from Movies m
+    join MovieRating mr
+        on m.movie_id = mr.movie_id
+    WHERE mr.created_at >= '2020-02-01'
+      AND mr.created_at < '2020-03-01'
+    group by m.movie_id, m.title
+    order by AVG(mr.rating) desc, m.title
+    limit 1
+);
