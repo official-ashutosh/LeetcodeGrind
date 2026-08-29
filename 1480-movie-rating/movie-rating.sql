d52d8ea8b61b7c504981f983bbd280ff6f886ff0
Ashutosh Kumar <145090058+official-ashutosh@users.noreply.github.com>
2026-08-29 17:50:56 +0530

    Time: 1248 ms (91.69%) | Memory: 0B (100.00%) - LeetSync

--- 1480-movie-rating/movie-rating.sql
+++ 1480-movie-rating/movie-rating.sql
@@ -3,7 +3,7 @@ select (
     select u.name
     from Users u
     join MovieRating mr
-        on u.user_id = mr.user_id
+    on u.user_id = mr.user_id
     group by u.user_id, u.name
     order by COUNT(*) desc, u.name
     limit 1
@@ -15,9 +15,8 @@ select (
     select m.title
     from Movies m
     join MovieRating mr
-        on m.movie_id = mr.movie_id
-    WHERE mr.created_at >= '2020-02-01'
-      AND mr.created_at < '2020-03-01'
+    on m.movie_id = mr.movie_id
+    where date_format(mr.created_at, '%Y-%m') = '2020-02'
     group by m.movie_id, m.title
     order by AVG(mr.rating) desc, m.title
     limit 1
