a1a17fcab98a5a05d06ba0a1fc7d31553e7ab8b0
Ashutosh Kumar <145090058+official-ashutosh@users.noreply.github.com>
2026-09-03 16:17:57 +0530

    Time: 4 ms (65.18%) | Memory: 165.9 MB (31.94%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
@@ -7,7 +7,7 @@ public:
             else mn2 = min(mn2, i);
         }
 
-        if(mn1 == INT_MAX || mn2 == INT_MAX) return true;
+        if(mn1 == INT_MAX) return true;
         if(mn1 > mn2) return false;
         return true;
     }
