818a6402d88ab5eb955922f016dd9322b986b23f
Ashutosh Kumar <145090058+official-ashutosh@users.noreply.github.com>
2026-09-03 16:17:17 +0530

    Time: 4 ms (65.18%) | Memory: 165.9 MB (62.83%) - LeetSync

---
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        int mn1 = INT_MAX, mn2 = INT_MAX;
+        for(auto i : nums1){
+            if(i % 2) mn1 = min(mn1, i);
+            else mn2 = min(mn2, i);
+        }
+
+        if(mn1 == INT_MAX || mn2 == INT_MAX) return true;
+        if(mn1 > mn2) return false;
+        return true;
+    }
+};
