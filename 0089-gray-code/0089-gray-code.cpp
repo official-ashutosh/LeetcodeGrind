aa6e9f181c48a7f9ec3a784d101cdaa7e8ed9331
Ashutosh Kumar <145090058+official-ashutosh@users.noreply.github.com>
2026-09-19 19:27:17 +0530

    Time: 4 ms (12.85%), Space: 22.4 MB (5.64%) - LeetHub

--- 0089-gray-code/0089-gray-code.cpp
+++ 0089-gray-code/0089-gray-code.cpp
@@ -1,12 +1,39 @@
 class Solution {
 public:
-    vector<int> grayCode(int n) {
-        vector<int> ans;
-        
-        for(int i=0; i < (1<<n); i++){
-            ans.push_back(i ^ (i >> 1));
+    vector<int> ans;
+    vector<bool> used;
+
+    bool backtrack(int cr, int n) {
+        if(ans.size() == (1 << n)){
+            return __builtin_popcount(ans.back() ^ ans[0]) == 1;
         }
-        
+
+        for(int i=0; i<n; i++){
+            int nxt = cr ^ (1<<i);
+
+            if(!used[nxt]){
+                used[nxt] = true;
+                ans.push_back(nxt);
+
+                if(backtrack(nxt, n)) return true;
+
+                ans.pop_back();
+                used[nxt] = false;
+            }
+        }
+
+        return false;
+    }
+
+    vector<int> grayCode(int n) {
+        int m = 1 << n;
+
+        used.assign(m, false);
+        ans.push_back(0);
+        used[0] = true;
+
+        backtrack(0, n);
+
         return ans;
     }
 };
